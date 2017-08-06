34dcd3537341958253fa73b82d5ed2771937de75
Herbey Zepeda <zepedaherbey@gmail.com>
2017-08-06 16:20:34 -0400

    Last batch of files

--- Crowe/HelloWorld/Client/Program.cs
+++ Crowe/HelloWorld/Client/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,20 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            string result = string.Empty;
+            //change this url as necessary, e.g. you might want to host the webAPI on IIS
+            string url = @"http://localhost:52035/api/operations";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                result = reader.ReadToEnd();
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
---
+++ Crowe/HelloWorld/Readme.txt
@@ -0,0 +1,13 @@
+
+Pull code from https://github.com/zepedaherbey/Demos
+Open a console application and type cd Demos\Crowe\HelloWorld\Client\bin\Debug
+Run the application by typeing 
+
+IMPORTANT
+If the applicaiton is not runnign you might have to change the url in Client.Program.cs.
+
+To do this right click on the WebApi project and select debug. A web browser should pop up.
+Copy the uri string and replace the following substring in Program.cs 
+http://localhost:52035/
+
+with whatever value you have copied.
